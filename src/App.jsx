870d5751355c25b8a6636920417254e6e2894326
Airshock72 <gelagela2222@gmail.com>
2024-04-10 13:50:40 +0400

    created reusable button and link component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
+import ButtonGradient from './assets/svg/ButtonGradient.jsx'
+import Button from './components/Button.jsx'
 
+const App = () =>  {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!!!!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <div className='pt-[4.75em] lg:pt-[5.25em] overflow-hidden'>
+        <Button className='mt-10' href='#login'>
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
   )
 }
 
